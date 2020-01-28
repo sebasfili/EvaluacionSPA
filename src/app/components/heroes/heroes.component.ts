3997108228daa5083c00f6dd24033204e8c07efb
sebasFili <sebastianfilippelli@gmail.com>
2020-01-27 23:03:55 -0300

    ver heroe tarjeta

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html'
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:any[] = [];
+
+  constructor( private _heroesService:HeroesService,
+               private router:Router ) {
+
+  }
+
+  ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+    //console.log(this.heroes);
+  }
+
+  verHeroe(idx:number){
+    this.router.navigate(['/heroe',idx])
+    //console.log(idx);
+  }
+
+}
